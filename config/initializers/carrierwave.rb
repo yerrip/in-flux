665d6cd7c8cbc021a448a4f341fa2ccf2965a18f
yerrip <yportill@lesley.edu>
2018-03-03 09:27:10 -0500

    set up first version of in-flux

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     'AKIAIHLBXGIPN5IXOTKA',                        # required
-    aws_secret_access_key: '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',                        # required
+    aws_access_key_id:     Rails.application.secrets.aws_key,                        # required
+    aws_secret_access_key: Rails.application.secrets.aws_secret,                        # required
   }
-  config.fog_directory  = 'in-flux-development'                                   # required
+  config.fog_directory  = Rails.application.secrets.aws_bucket                                   # required
 end

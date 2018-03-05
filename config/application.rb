60f9830faa3739f15d59d68fc38f549a0fcf09a1
yerrip <yportill@lesley.edu>
2018-03-05 17:06:23 -0500

    styling homepage hero and nav

--- config/application.rb
+++ config/application.rb
@@ -11,11 +11,11 @@ module Defekto
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.assets.enabled = true
+	config.assets.path << Rails.root.join('/app/assets/fonts')
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
 end
-
-config.assets.enabled = true
-config.assets.path << Rails.root.join('/app/assets/fonts')

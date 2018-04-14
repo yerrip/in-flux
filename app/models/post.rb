e28a356994baf20084762d10b364f6377ceb916c
yerrip <yportill@lesley.edu>
2018-04-14 09:21:01 -0400

    Adding form and new homepage

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ApplicationRecord
+	
+	validates :title, presence: true
+	validates :body, length: { minimum: 10 }
+	validates :score, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
+
 	mount_uploader :image, PostImageUploader
 end
